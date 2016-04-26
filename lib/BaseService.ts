5aecb97de1eead3caf928486d48695d861470fe0
Carestream <carestream@Aruls-MBP.corp.carestreamhealth.com>
2016-04-26 13:13:51 -0400

    Base Service added

--- lib/BaseService.ts
+++ lib/BaseService.ts
@@ -7,7 +7,7 @@ class BaseService {
      * @param {String} status - Response status.
      * @param {Object} message - The response object of the API request.
      */
-    public getServiceResponse (status, message) {
+    public createServiceResponse (status, message) {
 
         var _response = new ServiceResponse(status, message)
         return _response;
